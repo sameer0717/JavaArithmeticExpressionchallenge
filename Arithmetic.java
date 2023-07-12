e6bc3ed6b77ce46b171cb8c12bbf0f06a362c2a5
sameer0717 <70088228+sameer0717@users.noreply.github.com>
2023-07-12 03:59:11 -0700

    Add files via upload
    This file contains a code which performs arithmetic operations on different data types and has a description as well of what would be the data type of the outcome after the operation.

---
+++ Arithmetic.java
@@ -0,0 +1,32 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+// This is just a simple Arithmetic operation challenge
+
+        int a =10;
+        byte b = 12;
+        float c = 13.44f;
+        double d = 13.25d;
+        long e = 14;
+
+        //The bigger data type will be the outcome of the expression as result
+
+        //adding int and byte
+        System.out.println((a+b)); //this gives int result
+        //adding int and float
+        System.out.println(a+c);//It return float result
+        //adding int and double
+        System.out.println(a+d);//it return double result
+        //adding double and float
+        System.out.println(c+d);//The result will be double
+        //adding long and double
+        System.out.println(e+d);//This gives Double
+
+
+
+
+
+    }
+}
